93950fe247676815848fb71e2625d4d86e5a115a
Jack Yarndley <34801340+jackyarndley@users.noreply.github.com>
2026-08-06 19:09:43 +1200

    Add wrapped longitude intervals and geographic/projected limit APIs

--- test/runtests.jl
+++ test/runtests.jl
@@ -20,6 +20,7 @@ Makie.set_theme!(Theme(
     @testset "SphereClip" include("sphere_clip.jl")
     @testset "PR381Fixes" include("pr381_fixes.jl")
     @testset "GeoAxisV2" include("geoaxis_v2.jl")
+    @testset "WrappedLimits" include("wrapped_limits.jl")
     @testset "MeshImage" include("meshimage.jl")
     @testset "GeoAxis" include("geoaxis.jl")
     @testset "PolarGeoAxis" include("polar_geoaxis.jl")
